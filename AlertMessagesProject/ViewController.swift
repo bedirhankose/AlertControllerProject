17725e25555e9317aab77965a214282a70e336e9
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-21 00:19:18 +0100

    Initial Commit

---
+++ AlertMessagesProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertMessagesProject
+//
+//  Created by Bedirhan Köse on 21.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
